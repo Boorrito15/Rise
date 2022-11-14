be09146c99a67d62380e4232a77e14c1e204333c
claire paterson <patersonclaire@yahoo.co.uk>
2022-11-14 15:56:20 +0800

    FEAT: added dragon model

---
+++ db/migrate/20221114071324_create_dragons.rb
@@ -0,0 +1,16 @@
+class CreateDragons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dragons do |t|
+      t.string :name
+      t.string :species
+      t.integer :rating
+      t.integer :speed
+      t.string :fire_power
+      t.string :armour
+      t.text :description
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
