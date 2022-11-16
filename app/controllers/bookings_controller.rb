cedd68739467a58d601ab4a052cdb906e2621501
Alden Wahsono <aldenwahsono15@icloud.com>
2022-11-16 23:04:05 +0800

    hotfix

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,23 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
   def index
     @bookings = Booking.all
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @dragon = Booking.new(dragon_params)
+    @booking.user = current_user
+    if @dragon.save
+      redirect_to dragon_path(@dragon)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
 end
