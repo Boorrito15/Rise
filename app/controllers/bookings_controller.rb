18070c1ac4b423cf68301fee551203d31c9293e5
Alden Wahsono <aldenwahsono15@icloud.com>
2022-11-17 22:24:18 +0800

    Thursday night pre-sprint

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,26 +1,10 @@
 class BookingsController < ApplicationController
-<<<<<<< HEAD
-  before_action :set_booking, only: [:show]
-  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
-=======
-  before_action :set_booking, only: [:show, :edit, :update, :create]
-  before_action :set_dragon, only: [:create]
->>>>>>> e454733909b0ff2962472efc7b85e5d51fea42c9
+  before_action :set_booking, only: [:show, :edit, :update]
+  # before_action :set_dragon, only: [:create]
   def index
     @bookings = Booking.all
   end
 
-<<<<<<< HEAD
-  def new
-    @booking = Booking.new
-  end
-
-  def create
-    @dragon = Booking.new(dragon_params)
-    @booking.user = current_user
-    if @dragon.save
-      redirect_to dragon_path(@dragon)
-=======
   def show
   end
 
@@ -36,16 +20,12 @@ class BookingsController < ApplicationController
     @dragon = Dragon.find(params[:dragon_id])
     @booking.dragon = @dragon
     if @booking.save
-      redirect_to booking_path(@booking)
->>>>>>> e454733909b0ff2962472efc7b85e5d51fea42c9
+      redirect_to dragon_booking_path(@dragon, @booking), notice: "BLA"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-<<<<<<< HEAD
-
-=======
   private
 
   def set_booking
@@ -55,5 +35,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :status, :departure_location, :arrival_location)
   end
->>>>>>> e454733909b0ff2962472efc7b85e5d51fea42c9
 end
